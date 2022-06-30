de40cf12c28234a0dd311a5b2098b4ec7d977c19
LuxshanPuvanendrarajah <107274553+LuxshanPuvanendrarajah@users.noreply.github.com>
2022-06-30 08:57:36 +0100

    Add files via upload

--- movilens_task.sql
+++ movilens_task.sql
@@ -7,11 +7,24 @@ ORDER BY release_date DESC;
 SELECT m.title,AVG(r.rating)
 FROM movies m
 JOIN ratings r ON m.id=r.movie_id 
-Group by m.title
+GROUP BY m.id
 ORDER BY AVG(r.rating) ASC;
 -- List the unique records for Sci-Fi movies where male 24-year-old students have given 5-star ratings.
-
-
+SELECT DISTINCT m.title , g.`name` ,r.rating, u.gender , u.age, o.`name` 
+FROM movies m
+JOIN genres_movies gm ON gm.movie_id = m.id
+JOIN ratings r ON r.movie_id = m.id
+JOIN genres g ON g.id = gm.genre_id
+JOIN users u ON u.id = r.user_id
+JOIN occupations o ON o.id = u.occupation_id 
+WHERE g.`name` = "Sci-Fi" AND u.age = 24 AND u.gender = "M" AND o.`name` = "Student" AND r.rating = 5;
 -- List the unique titles of each of the movies released on the most popular release day.
 
+
 -- Find the total number of movies in each genre; list the results in ascending numeric order.
+SELECT g.`name`, COUNT(m.id) 
+FROM movies m
+JOIN genres_movies gm ON gm.movie_id = m.id
+JOIN genres g ON g.id = gm.genre_id
+GROUP BY g.`name`
+ORDER BY COUNT(m.id) ASC;
